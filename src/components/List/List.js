66123edd479e68659f8bf8b19a0cc9e75cf802b8
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-14 10:46:11 +0200

    Add 3 columns to List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 
 class List extends React.Component {
     static PropTypes = {
@@ -20,6 +21,11 @@ class List extends React.Component {
           <Hero titleText={this.props.title} image={this.props.image}/>
           <div className={styles.description}>
               {this.props.children}
+          </div>
+          <div className={styles.columns}>
+            <Column columnTitle={'first'}/>
+            <Column columnTitle={'second'}/>
+            <Column columnTitle={'third'}/>
           </div>
         </section>
       </div>
