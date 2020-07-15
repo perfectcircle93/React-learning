12a5bfd8537a93fca79c4cd2f33001a1e2159d4e
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-15 09:44:14 +0200

    Add component state

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -7,6 +7,9 @@ import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
     static PropTypes = {
         title: PropTypes.node.isRequired,
         description: PropTypes.node,
@@ -26,9 +29,9 @@ class List extends React.Component {
               {ReactHtmlParser(this.props.description)}
           </div>
           <div className={styles.columns}>
-            <Column columnTitle={'first'}/>
-            <Column columnTitle={'second'}/>
-            <Column columnTitle={'third'}/>
+            {this.state.columns.map(({key, ...columnProps}) => (
+            <Column key={key} {...columnProps} />
+            ))}
           </div>
         </section>
       </div>
