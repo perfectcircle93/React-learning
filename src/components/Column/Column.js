244d0106659c723885e9949c48e28e536ccbec81
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-15 10:24:21 +0200

    Add Card.js and Icon.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import Card from '../Card/Card';
+import { settings } from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
+import Icon from '../Icon/Icon.js';
 
+class Column extends React.Component {
 
-const Column = props => (
-    <section className={styles.component}>
-        <h3 className={styles.title}>{props.columnTitle}</h3>
-    </section>
-);
+    state = {
+        cards: this.props.cards || [],
+    }
 
-Column.PropTypes = {
-    columnTitle: PropTypes.node.isRequired,
-};
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+    }
+
+    static defaultProps = {
+        icon: settings.defaultColumnIcon,
+    }
+
+    addCard(title) {
+        this.setState(state => (
+            {
+                cards: [
+                    ...state.cards,
+                    {
+                        key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+                        title,
+                        icon: 'list-alt',
+                        cards: []
+                    }
+                ]
+            }
+        ));
+    }
+
+    render() {
+        return (
+            <section className={styles.component}>
+                <h3 className={styles.title}>{this.props.title}
+                <span className={styles.icon}><Icon name={this.props.icon}/></span>
+                </h3>
+                <div>
+                    {this.state.cards.map(({ key, ...cardsProps }) => (
+                        <Card key={key} {...cardsProps} />
+                    ))}
+                </div>
+                <div>
+                    <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+                </div>
+            </section>
+        )
+    }
+
+}
 
 export default Column;
