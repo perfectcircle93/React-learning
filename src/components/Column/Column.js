f8a15af0bf71df99329c9025fcc7fd8d41d3c18f
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-17 17:00:17 +0200

    Prepare the column component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import Card from '../Card/Card';
+//import Card from '../Card/Card';
 import { settings } from '../../data/dataStore';
-import Creator from '../Creator/Creator.js';
+//import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
-
-    state = {
-      cards: this.props.cards || [],
-    }
 
     static propTypes = {
       title: PropTypes.node.isRequired,
@@ -20,36 +16,22 @@ class Column extends React.Component {
       icon: settings.defaultColumnIcon,
     }
 
-    addCard(title) {
-      this.setState(state => (
-        {
-          cards: [
-            ...state.cards,
-            {
-              key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
-              title,
-              icon: 'list-alt',
-              cards: [],
-            },
-          ],
-        }
-      ));
-    }
 
     render() {
+      const {title, icon} = this.props;
       return (
         <section className={styles.component}>
-          <h3 className={styles.title}>{this.props.title}
-            <span className={styles.icon}><Icon name={this.props.icon}/></span>
+          <h3 className={styles.title}>{title}
+            <span className={styles.icon}><Icon name={icon}/></span>
           </h3>
-          <div>
+          {/*<div>
             {this.state.cards.map(({ key, ...cardsProps }) => (
               <Card key={key} {...cardsProps} />
             ))}
           </div>
           <div>
             <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-          </div>
+            </div>*/}
         </section>
       );
     }
@@ -57,7 +39,7 @@ class Column extends React.Component {
 }
 
 Column.propTypes = {
-  cards: PropTypes.node,
+  title: PropTypes.node,
   icon: PropTypes.node,
 };
   
