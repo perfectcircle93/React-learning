eebb0f18cbfb19c01c5803bb40a28c65a96429a3
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-18 12:34:24 +0200

    Add type and action creator into searchStringRedux

--- redux/searchStringRedux.js
+++ redux/searchStringRedux.js
@@ -1,3 +1,5 @@
+import shortid from 'shortid';
+
 // selectors
 export const getSearchString = ({ searchString }) => searchString;
 
@@ -6,10 +8,14 @@ export const countAllCards = ({ cards }) => cards.length;
 export const countVisibleCards = ({ cards, searchString }) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
+const reducerName = 'cards';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const ADD_CARD = createActionName('ADD_CARD');
 
 // action creators
+export const createActionAddCard = payload => ({ payload: { ...payload, id: shortid.generate() }, type: ADD_CARD });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
